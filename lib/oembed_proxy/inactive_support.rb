bca1c5e92854981708e1fc6c9db4b4f57f3abebb
William Johnston <wjohnston@mpr.org>
2018-01-26 15:19:41 -0800

    Pull in activesupport stuff

---
+++ lib/oembed_proxy/inactive_support.rb
@@ -0,0 +1,21 @@
+module OembedProxy
+  # A couple of things pulled in from ActiveSupport, but using refinements
+  # instead of monkeypatching.
+  module InactiveSupport
+    refine String do
+      def parameterize(sep = '-')
+        parameterized_string = self.dup.downcase
+        # Turn unwanted chars into the separator
+        parameterized_string.gsub!(/[^a-z0-9\-_]+/, sep)
+        unless sep.nil? || sep.empty?
+          re_sep = Regexp.escape(sep)
+          # No more than one of the separator in a row.
+          parameterized_string.gsub!(/#{re_sep}{2,}/, sep)
+          # Remove leading/trailing separator.
+          parameterized_string.gsub!(/^#{re_sep}|#{re_sep}$/, '')
+        end
+        parameterized_string.downcase
+      end
+    end
+  end
+end
