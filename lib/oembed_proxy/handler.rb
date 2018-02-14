85ea6d482b6547c89c40968f964eaabee1b3a0d1
William Johnston <wjohnston@mpr.org>
2018-02-14 11:26:26 -0600

    Add main handler

---
+++ lib/oembed_proxy/handler.rb
@@ -0,0 +1,27 @@
+module OembedProxy
+  class Handler
+    def initialize(providers = [])
+      @registered_providers = providers
+    end
+
+    def register(provider)
+      @registered_providers << provider
+    end
+
+    def handles_url?(url)
+      !provider_for_url(url).nil?
+    end
+
+    def get_data(url, other_params = {})
+      provider = provider_for_url(url)
+      return provider.get_data(url, other_params) unless provider.nil?
+    end
+
+    private
+
+    def provider_for_url(url)
+      # TODO: Switch to #find
+      @registered_providers.select { |p| p.handles_url? url }.first
+    end
+  end
+end
---
+++ spec/oembed_proxy/handler_spec.rb
@@ -0,0 +1,26 @@
+require 'oembed_proxy/handler'
+
+RSpec.describe OembedProxy::Handler do
+  let(:handler) { described_class.new }
+  it 'composes multiple providers' do
+    handler.register double('Provider1', get_data: 'data1', handles_url?: false)
+    handler.register double('Provider2', get_data: 'data2', handles_url?: true)
+
+    expect(handler.handles_url?('test')).to eq true
+    expect(handler.get_data('test')).to eq 'data2'
+  end
+
+  it 'works with a single provider' do
+    handler.register double('Provider1', get_data: 'data1', handles_url?: true)
+
+    expect(handler.handles_url?('test')).to eq true
+    expect(handler.get_data('test')).to eq 'data1'
+  end
+
+  it 'fails gracefully' do
+    handler.register double('Provider1', get_data: 'data1', handles_url?: false)
+
+    expect(handler.handles_url?('test')).to eq false
+    expect(handler.get_data('test')).to eq nil
+  end
+end
