37b127d38a583b407b38d53e1ffcc8497288d8e0
William Johnston <wjohnston@mpr.org>
2018-02-15 09:16:23 -0600

    Include all of the providers by default

--- lib/oembed_proxy.rb
+++ lib/oembed_proxy.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require 'oembed_proxy/version'
+require 'oembed_proxy/handler'
+require 'oembed_proxy/associated_press'
+require 'oembed_proxy/embedly'
+require 'oembed_proxy/first_party'
+require 'oembed_proxy/fusiontable_map'
+require 'oembed_proxy/google_document'
+require 'oembed_proxy/google_mapsengine'
+require 'oembed_proxy/google_spreadsheet'
+require 'oembed_proxy/tableau'
 
 # Gem namespace
 module OembedProxy
