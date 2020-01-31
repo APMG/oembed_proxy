74c982077666ae4e7195b58cd5b5f539c98ddc62
Geoff Hankerson <ghankerson@mor.org>
2020-01-31 09:39:43 -0600

    code review  feedback addressed

--- lib/oembed_proxy/npr.rb
+++ lib/oembed_proxy/npr.rb
@@ -15,6 +15,7 @@ module OembedProxy
     def get_data(url, _other_params = {}) # rubocop:disable Metrics/MethodLength
       return nil unless handles_url? url
 
+      escaped_url = url.gsub('"', '&quot;')
       {
         'type' => 'rich',
         'version' => '1.0',
@@ -22,7 +23,7 @@ module OembedProxy
         'provider_url' => 'https://www.npr.org/',
         'html' => <<~HTML,
           <div class='sidechain-wrapper'>
-            <side-chain src="#{url}"></side-chain>
+            <side-chain src="#{escaped_url}"></side-chain>
           </div>
         HTML
       }
